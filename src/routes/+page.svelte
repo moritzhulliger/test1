ffca89fc67dbb126c48f15ffa014c38d84c8e577
huli <moritz.hulliger@gmail.com>
2024-01-06 21:06:36 +0100

    add bg image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,5 +3,17 @@
    console.log(welcomeMessage)
 </script>
 
+<div class="main-container">
+
+</div>
 <h1>{welcomeMessage}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+ .main-container {
+    width: 100vw;
+    height: 50vh;
+    background-image: url(/static/images/main1.jpg);
+    background-size: contain;
+ }
+</style>
