3c4d67a28676302a0247dd04454267ce6d4a8856
huli <moritz.hulliger@gmail.com>
2024-01-06 22:00:29 +0100

    some

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,30 @@
 <script>
-   export const welcomeMessage = "We Eloped"
+   export const welcomeMessage = "Thuy-Duc \n & Moritz"
    console.log(welcomeMessage)
 </script>
 
 <div class="main-container">
+    <h1>{welcomeMessage}</h1>
 
 </div>
-<h1>{welcomeMessage}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <style>
  .main-container {
+    position: absolute;
+
     width: 100vw;
     height: 50vh;
-    background-image: url(/static/images/main1.jpg);
-    background-size: contain;
+    min-height: 60vw;
+    background-image: url(/src/lib/images/main1.jpg);
+    background-size: cover;
+ }
+
+ h1 {
+    font-weight: 100;
+    font-size: 5rem;
+    width: 40vw;
+    margin: 50px auto 0;
+    text-align: center;
  }
 </style>
