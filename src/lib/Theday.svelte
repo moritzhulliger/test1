fb81762e1b47cd9b21297a481ab82975bab16e42
Thuy-Duc <joyce.daoto@gmail.com>
2024-01-06 23:15:17 +0100

    add some title and subtitle

--- src/lib/Theday.svelte
+++ src/lib/Theday.svelte
@@ -1,20 +1,13 @@
 <script>
-import {
-	Timeline,
-	TimelineItem,
-	TimelineSeparator,
-	TimelineDot,
-	TimelineConnector,
-	TimelineContent,
-	TimelineOppositeContent
-} from 'svelte-vertical-timeline';
+
 
 const options = [
-		{ title: 'Eat', time: '09:30 am' },
-		{ title: 'Sleep', time: '10:00 am' },
-		{ title: 'Code', time: '11:00 am' },
-		{ title: 'Eat', time: '01:00 pm' }
+		{ title: 'Frist Course Wedding Dinner', subtitle: '09:30 am' },
+		{ title: 'Sleep', subtitle: '10:00 am' },
+		{ title: 'Code', subtitle: '11:00 am' },
+		{ title: 'Eat', subtitle: '01:00 pm' }
 	];
 </script>
 
 <h2>Our Special Days</h2>
+ 
