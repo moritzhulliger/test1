e313e8eea96aecfc497a45cb043abc8348fd0efc
huli <moritz.hulliger@gmail.com>
2024-01-07 21:48:28 +0100

    move amply

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,7 @@
 // place files you want to import through the `$lib` alias in this folder.
 import { Amplify } from 'aws-amplify';
 import { generateClient } from 'aws-amplify/api';
-import config from '../amplifyconfiguration.json';
+import config from './amplifyconfiguration.json';
 Amplify.configure(config);
 
 export const client = generateClient();
