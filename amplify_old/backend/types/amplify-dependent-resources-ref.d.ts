a8cc4d58902a18174f995cbf42d1eac98483eae2
huli <moritz.hulliger@gmail.com>
2024-09-02 20:33:26 +0200

    trigga

---
+++ amplify_old/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,15 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "elope": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "storage": {
+    "s3elopestorage45bad612": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
